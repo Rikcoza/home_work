009d17a2cdf8600cca900e81f354492f47bc95fa
unknown <pacharagame555@gmail.com>
2023-10-15 11:24:19 +0700

    homework

---
+++ study_1.dart
@@ -0,0 +1,47 @@
+
+void main(){
+print('.....................................................................');
+homework_1();
+print('.....................................................................');
+homework_2();
+print('.....................................................................');
+homework_3();
+print('.....................................................................');
+homework_4();
+print('.....................................................................');
+}
+
+ 
+void homework_1(){
+int sum_1 = 0  ;
+for(var count_1 = sum_1 ; count_1 <= 20 ;count_1++){
+  print('$count_1');
+  }
+}
+
+void homework_2(){
+  int sum_2 = 0 ;
+  for(var count_2 = sum_2 ; count_2 <= 20 ;count_2++){
+if(count_2 % 2 == 0){
+  print('$count_2');
+} 
+}
+}
+
+void homework_3(){
+  int sum_3 = 0 ;
+  for(var count_3 = sum_3 ; count_3 <= 20 ;count_3++){
+if(count_3 % 2 == 1){
+  print('$count_3');
+} 
+}
+}
+
+void homework_4(){
+  int sum_4 = 0 ;
+  for(var count_4 = sum_4 ; count_4 <= 20 ;count_4++){
+if(count_4 % 3 == 0){
+  print('$count_4');
+} 
+}
+}
